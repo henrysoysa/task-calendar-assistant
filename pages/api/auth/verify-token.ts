2ba941344cf64dd0b69bd3e5aaf3a528fcf13d75
Henry Soysa <henry.soysa@gmail.com>
2024-10-21 16:51:53 +0200

    updating firebase in env file for deployment

--- pages/api/auth/verify-token.ts
+++ pages/api/auth/verify-token.ts
@@ -1,11 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../../right-hand-cal-firebase-admin.json';
 
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
     });
     console.log('Firebase Admin initialized successfully');
   } catch (error) {
