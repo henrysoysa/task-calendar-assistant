545d3e7ea3ed088e8da7c92ff123ea7544ed5819
Henry Soysa <henry.soysa@gmail.com>
2024-10-29 12:44:56 +0100

    removing firebase auth references

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    runtime: 'nodejs',
+  },
   env: {
     // Remove all Firebase-related environment variables
   },
